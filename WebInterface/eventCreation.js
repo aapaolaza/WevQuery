53274df07354c8526e9a42e5656288171b7088e1
Aitor Apaolaza <aitor.apaolaza@manchester.ac.uk>
2016-11-29 16:49:46 +0000

    Fixed error when dragging elements from palette

--- WebInterface/eventCreation.js
+++ WebInterface/eventCreation.js
@@ -1,1 +1,6 @@
-function addNewEvent()
+
+/** When the user presses in the 'plus' sign of the "New event" object, a new event will be created.
+ * This event won't have an ID (the ID will be set when it)
+*/
+
+//function addNewEvent()
