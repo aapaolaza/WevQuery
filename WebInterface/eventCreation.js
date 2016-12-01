72810dd0ccfb345caf1f7f36e4c6adca4adde58b
Aitor Apaolaza <aitor.apaolaza@manchester.ac.uk>
2016-12-01 12:15:13 +0000

    Added new event dialog Fixed jquery-ui css issues

--- WebInterface/eventCreation.js
+++ WebInterface/eventCreation.js
@@ -3,21 +3,64 @@
  * This event won't have an ID (the ID will be set when it)
 */
 
-function addNewEvent(){
+function addEventTemplate() {
+  var valid = true;
+  allFields.removeClass("ui-state-error");
 
-  var event = $("<li class='eventTemplate eventTemplateDraggable'><div class='title'> Event template <span class='dragIcon' aria-hidden='true' aria-label='Move Event'></span></div> <p> Events:</p><p class='eventList'></p><p> Occurrence:</p><p class='occurrence'></p><p> Context</p><p class='contextItem'></p></li>");
+  valid = valid && checkLength(name, "username", 3, 16);
+  valid = valid && checkLength(email, "email", 6, 80);
+  valid = valid && checkLength(password, "password", 5, 16);
 
-  $('.title', event).attr('id','a1234');  // set the attribute
-  $('.eventList', event).text("mousemove,mouseout");  // set the attribute 
-  
-  $('body').append(e); // put it into the DOM
+  valid = valid && checkRegexp(name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter.");
+  valid = valid && checkRegexp(email, emailRegex, "eg. ui@jquery.com");
+  valid = valid && checkRegexp(password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9");
+
+  if (valid) {
+    $("#users tbody").append("<tr>" +
+      "<td>" + name.val() + "</td>" +
+      "<td>" + email.val() + "</td>" +
+      "<td>" + password.val() + "</td>" +
+      "</tr>");
+    dialog.dialog("close");
+  }
+  return valid;
+}
+
+function updateTips(t) {
+  tips
+    .text(t)
+    .addClass("ui-state-highlight");
+  setTimeout(function () {
+    tips.removeClass("ui-state-highlight", 1500);
+  }, 500);
+}
+
+function checkLength(o, n, min, max) {
+  if (o.val().length > max || o.val().length < min) {
+    o.addClass("ui-state-error");
+    updateTips("Length of " + n + " must be between " +
+      min + " and " + max + ".");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function checkRegexp(o, regexp, n) {
+  if (!(regexp.test(o.val()))) {
+    o.addClass("ui-state-error");
+    updateTips(n);
+    return false;
+  } else {
+    return true;
+  }
 }
 
 /**
  * To be called when an event is dragged from the palette to the ordered area.
  * Will set a unique ID for that event. Possibly some additional tasks as well.
  */
-function addEventToOrderedList(){
+function addEventToOrderedList() {
 
 }
 
@@ -25,6 +68,6 @@ function addEventToOrderedList(){
  * To be called when a new temporal constraint is to be created.
  * It shows a dialog that contains the various necessary options.
  */
-function addTemporalConstraint(){
-  
+function addTemporalConstraint() {
+
 }
